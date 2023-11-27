325ae81a7d471f918bb3e82be8786adac977d0ad
Maycon Felix Guarda <104796534+mayconfguarda@users.noreply.github.com>
2023-11-27 16:30:32 -0300

    alterado a logica, aguarda 200 ok

--- getcnpj.py
+++ getcnpj.py
@@ -4,26 +4,34 @@ import pandas as pd
 from datetime import datetime
 from json.decoder import JSONDecodeError
 from requests.exceptions import RequestException
+import time
 
 def consultar_api_cnpj(cnpj):
     url = f'https://receitaws.com.br/v1/cnpj/{cnpj}'
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        dados = response.json()
-        return dados
-    except RequestException as e:
-        print(f"Erro ao consultar CNPJ {cnpj}: {e}")
-        return None
-    except JSONDecodeError:
-        print(f"Erro ao decodificar JSON para CNPJ {cnpj}")
-        return None
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                dados = response.json()
+                return dados
+            elif response.status_code == 429:  # Too Many Requests
+                print(f"API retornou 'Too many requests'. Aguardando até obter status 200...")
+            else:
+                print(f"Erro ao consultar CNPJ {cnpj}. Status Code: {response.status_code}")
+            # Aguarda um curto período de tempo antes de tentar novamente
+            time.sleep(1)
+        except RequestException as e:
+            print(f"Erro ao consultar CNPJ {cnpj}: {e}")
+            # Aguarda um curto período de tempo antes de tentar novamente
+            time.sleep(1)
+        except JSONDecodeError:
+            print(f"Erro ao decodificar JSON para CNPJ {cnpj}")
+            # Aguarda um curto período de tempo antes de tentar novamente
+            time.sleep(1)
 
 def criar_excel(dados, output_file='dados_empresas.xlsx'):
     if not os.path.exists(output_file):
-        # Se o arquivo não existe, crie um novo com os dados
         df = pd.DataFrame(columns=['CNPJ', 'Nome', 'Email'])
-
         if dados:
             nova_linha = {
                 'CNPJ': dados.get('cnpj', ''),
@@ -35,9 +43,7 @@ def criar_excel(dados, output_file='dados_empresas.xlsx'):
         df.to_excel(output_file, index=False, engine='openpyxl')
         print(f'Dados salvos em {output_file}')
     else:
-        # Se o arquivo já existe, carregue-o, adicione uma nova linha e salve-o novamente
         df_existente = pd.read_excel(output_file, engine='openpyxl')
-
         if dados:
             nova_linha = {
                 'CNPJ': dados.get('cnpj', ''),
@@ -56,10 +62,14 @@ def processar_cnpjs_arquivo(arquivo, arquivo_saida):
 
     for cnpj in cnpjs:
         dados_empresa = consultar_api_cnpj(cnpj)
+        while dados_empresa is None:
+            dados_empresa = consultar_api_cnpj(cnpj)
+
         criar_excel(dados_empresa, arquivo_saida)
+        time.sleep(0.5)
 
 def main():
-    arquivo_cnpjs = 'cnpjs.txt'  # Substitua pelo nome do seu arquivo de CNPJs
+    arquivo_cnpjs = 'cnpjs.txt'
     data_atual = datetime.now().strftime("%Y%m%d%H%M%S")
     arquivo_saida = f'dados_empresas_{data_atual}.xlsx'
     processar_cnpjs_arquivo(arquivo_cnpjs, arquivo_saida)
